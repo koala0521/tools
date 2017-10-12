ecc310a7f76bf55a6793c9d972a51f4f29133222
unknown <gaopengfei@mvwchina.com>
2017-10-12 20:01:18 +0800

    增加extend方法

--- js/tools_fun/tools-until.js
+++ js/tools_fun/tools-until.js
@@ -15,7 +15,7 @@
         if (typeof arg !== 'object') {
             return false;
         }
-        str = Object.prototype.tostring.call(arg);
+        str = Object.prototype.toString.call(arg);
         return str === '[object Array]';
     }
 
@@ -25,15 +25,15 @@
         if (typeof arg !== 'object') {
             return false;
         }
-        str = Object.prototype.tostring.call(arg);
+        str = Object.prototype.toString.call(arg);
         return str === '[object Object]';
     }
 
     //检测空对象
     pf.isEmatyObjset = function(arg) {
         var len = 0;
-        if (typeof arg !== 'object') {
-            throw new Error('"arg",is not a Object');
+        if (!pf.isObject(arg)) {
+            throw new Error('arg Error: "arg",is not a Object');
         }
         for (var key in arg) {
             if (object.hasOwnProperty(key)) {
@@ -43,4 +43,67 @@
         return !!len;
     }
 
+    //检测函数
+    pf.isFunction = function(arg) {
+        return !!(typeof arg === 'function');
+    }
+
+    //复制对象
+    pf.extend = function() {
+        var i = 1,
+            deep = false,
+            len = arguments.length,
+            arg,
+            key,
+            src,
+            copy,
+            clone,
+            target = arguments[0] || {}; //合并后的对象
+        if (typeof target === 'boolean') { //深度拷贝
+
+            deep = arguments[0];
+            target = arguments[i] || {};
+            i++;
+        }
+        if (typeof target !== 'object' && !Elf.utils.isFunction(target)) {
+
+            target = {};
+        }
+        if (i === len) { //只传入一个非布尔值的参数，或者传入一个布尔值和一个其他参数时
+            target = window;
+            i--;
+        }
+        for (; i < len; i++) {
+            if (arguments[i] !== null) {
+                arg = arguments[i]; //待合并的每一个对象
+                for (key in arg) { //遍历这个对象
+                    if (arg.hasOwnProperty(key)) { //检测是否是自身的属性
+                        src = target[key]; //源对象该属性
+                        copy = arg[key]; //待复制对象的该属性
+                        if (target === copy) { //源对象和 带复制的对象的属性是同一个对象
+
+                            continue; //跳出循环，进行下一次复制
+                        }
+                        if (deep && copy && (pf.isObject(copy) || pf.isArray(copy))) {
+
+                            if (pf.isArray(copy)) {
+                                clone = src && pf.isArray(src) ? src : []; //源对象属性为数组，把目标数组复制到源对象子数组
+                            } else {
+                                clone = src && pf.isObject(src) ? src : {};
+                            }
+
+                            //递归复制
+                            target[key] = pf.extend(deep, clone, copy);
+
+                        } else if (copy !== undefined) { //非深度复制 || 目标不是对象或者数组，不需要深度复制
+                            target[key] = copy;
+                        }
+                    }
+                }
+            }
+
+        }
+
+        return target;
+    }
 })($pf);
